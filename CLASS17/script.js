eab641aa17926cfc41920300f7652cdcbab2a2e9
“Andrea <andream.allen35@gmail.com>
2018-11-05 12:37:42 -0500

    HW FizzBuzz

---
+++ CLASS17/script.js
@@ -0,0 +1,14 @@
+let num = 1
+while(num <101){
+	if(num % 3 === 0 && num % 5 === 0){
+		console.log(' FizzBuzz ');
+	}else if(num % 5 === 0)  { 
+		console.log(' Buzz ');
+	}else if( num % 3 === 0 ) {
+		console.log(' Fizz ')
+	}else {
+		console.log(num)
+	}
+	num++;
+}
+
