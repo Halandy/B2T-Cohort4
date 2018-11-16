234fcbdaee3c14285291890d5b91ad4940b334df
“Andrea <andream.allen35@gmail.com>
2018-11-16 15:36:00 -0500

    DisplayingNamesHW

---
+++ DisplayingNamesHW/script.js
@@ -0,0 +1,9 @@
+
+function getFormValue(){
+	let fname = document.getElementById('fname').value;
+	let lname = document.getElementById('lname').value;
+
+	fullName = fname + ' ' + lname;
+
+	document.getElementById('answerDisplay').innerHTML = 'Hello ' + fullName;
+}
